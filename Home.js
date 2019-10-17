5506c006cf25b643613ff88500349db22374c3e0
yeshvasan <48148176+yeshvasan@users.noreply.github.com>
2019-10-17 15:03:10 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { StyleSheet,View,Text, Button } from 'react-native';
+import { StyleSheet,View,Text, Button, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
+import {Entypo} from '@expo/vector-icons';
+import {  Card} from 'native-base';
 
 class Home extends React.Component {
     static navigationOptions ={
-      title:'Welcome to FriendList App..'
+      title:"Contact App"
     }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>We have { this.props.friends.current.length } friends!</Text>
-        <Button 
-          title="Add some friends"
-          fontWeight="bold"
-          color="#3C40C6"
-          onPress={() =>
+
+       <Card style={styles.listItem}> 
+          <View style={styles.infoContainer}>
+              <Text style={styles.infoText} >
+              You Successfully selected { this.props.friends.current.length } friends!
+              </Text>
+          </View>
+        </Card>
+        <TouchableOpacity style={styles.floatButton} onPress={() =>
             this.props.navigation.navigate('Friends')
-          }
-        />
+          }>
+            <Entypo name="plus" size={30} color="#fff" />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -27,15 +33,45 @@ class Home extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#53E0BC',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
   text:{
-      color:"#fff",
-      fontSize:20,
-      marginVertical:5
-  }
+      color:"#30336B",
+      fontSize:30,
+      marginVertical:5,
+      fontWeight:"bold"
+  },
+  floatButton: {
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 60,
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    height: 60,
+    backgroundColor: "#B83227",
+    borderRadius: 100
+  },
+  listItem: {
+    flexDirection: "row",
+    padding: 20,
+    backgroundColor:"#01CBC6",
+
+  },
+  infoContainer: {
+    flexDirection: "column",
+   
+  },
+  infoText: {
+    fontSize: 30,
+    fontWeight: "400",
+    paddingLeft: 10,
+    paddingTop: 2
+  },
   
 });
 
